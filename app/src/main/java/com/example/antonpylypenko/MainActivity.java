6b1431006f80d802c93f14e6d709a242fefe39d5
AntonPylypenko <anton.pylypenko5@gmail.com>
2019-09-18 23:09:47 +0300

    Trying to do right list

--- app/src/main/java/com/example/antonpylypenko/MainActivity.java
+++ app/src/main/java/com/example/antonpylypenko/MainActivity.java
@@ -12,11 +12,41 @@ public class MainActivity extends AppCompatActivity {
     private EditText pass;
     private Button btn;
 
+    private String[] gairaigoWords = {
+            "アベック - romantic couple",
+            "アフターサービス - after service",
+            "アイドル - (teen) idol, pop star",
+            "アイス - ice, also used as a shortened form of ice cream",
+            "アイスクリーム - ice cream",
+            "アイゼン - crampons",
+            "アメフト - American football",
+            "アメリカンドッグ - corn dog",
+            "アニメ - animation, animated cartoons or films",
+            "アニソン - an anime song, most often the theme",
+            "アンケート - questionnaire, survey",
+            "アンニュイ - ennui, boredom",
+            "アンサー - reply to a question, solution to a problem",
+            "アンチ - hater, anti-fan",
+            "アパート - apartment (US), flat (UK), though apāto are usually in small two-story wood-structure buildings, not multistory complexes as in the US usage"}
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    //Create and set up adapter
+    ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, players)
+    setListAdapter(adapter);
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        android.widget.Toast.makeText(this, getListView().getItemAtPosition(position).toString());
+    }
+
 
     public void addListenerOnButton(){
         pass = (EditText) findViewById(R.id.editText);
