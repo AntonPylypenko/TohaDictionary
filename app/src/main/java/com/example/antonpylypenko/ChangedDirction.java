165f075056a4bcc20ac0382c59de2af06f3af621
AntonPylypenko <anton.pylypenko5@gmail.com>
2019-10-01 22:12:58 +0300

    New activity is added

---
+++ app/src/main/java/com/example/antonpylypenko/ChangedDirction.java
@@ -0,0 +1,14 @@
+package com.example.antonpylypenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangedDirction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changed_dirction);
+    }
+}
